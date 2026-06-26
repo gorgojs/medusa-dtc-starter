e4556a06512911becba22fdd631888302fd5baa8
PoSayDone <poseaydone@ya.ru>
2026-06-26 20:35:23 +0500

    feat: telegram icon fix

--- apps/storefront/src/modules/cart/templates/index.tsx
+++ apps/storefront/src/modules/cart/templates/index.tsx
@@ -15,7 +15,7 @@ const CartTemplate = ({
       <div className="content-container" data-testid="cart-container">
         {cart?.items?.length ? (
           <div className="grid grid-cols-1 small:grid-cols-[7fr_5fr] min-h-[70vh] gap-x-20">
-            <div className="flex flex-col px-4 py-6 gap-y-6">
+            <div className="flex flex-col py-6 gap-y-6">
               <ItemsTemplate cart={cart} />
             </div>
             <div className="relative">
