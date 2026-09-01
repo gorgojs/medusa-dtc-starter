adf598724663bf9434a19303ffa3a07b56e7bdd1
ttokyose <parsifal.cs@gmail.com>
2026-09-01 11:11:07 +0300

    perf: replace lodash with a local shallow comparison

--- apps/storefront/src/lib/util/compare-addresses.ts
+++ apps/storefront/src/lib/util/compare-addresses.ts
@@ -1,28 +1,20 @@
-import { isEqual, pick } from "lodash"
+const ADDRESS_FIELDS = [
+  "first_name",
+  "last_name",
+  "address_1",
+  "company",
+  "postal_code",
+  "city",
+  "country_code",
+  "province",
+  "phone",
+] as const
 
 export default function compareAddresses(address1: object, address2: object) {
-  return isEqual(
-    pick(address1, [
-      "first_name",
-      "last_name",
-      "address_1",
-      "company",
-      "postal_code",
-      "city",
-      "country_code",
-      "province",
-      "phone",
-    ]),
-    pick(address2, [
-      "first_name",
-      "last_name",
-      "address_1",
-      "company",
-      "postal_code",
-      "city",
-      "country_code",
-      "province",
-      "phone",
-    ])
-  )
+  const a = address1 as Record<string, unknown> | null | undefined
+  const b = address2 as Record<string, unknown> | null | undefined
+
+  if (!a || !b) return a === b
+
+  return ADDRESS_FIELDS.every((field) => Object.is(a[field], b[field]))
 }
